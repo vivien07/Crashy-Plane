43615a9693e43a29ed9eed73cb26148d108c615c
vivien ruas <vivienruas07@gmail.com>
2019-11-16 12:23:30 +0100

    controls added contacts detected

--- Crashy Plane/GameScene.swift
+++ Crashy Plane/GameScene.swift
@@ -16,16 +16,32 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-        createPlayer()
+        //game setup
         createSky()
         createBackground()
         createGround()
+        createScore()
+        createPlayer()
         startRocks()
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -5.0)
+        physicsWorld.contactDelegate = self //A delegate that is called when two physics bodies come in contact with each other.
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+        let value = player.physicsBody!.velocity.dy * 0.001
+        let rotate = SKAction.rotate(toAngle: value, duration: 0.1)
+        player.run(rotate)
         
     }
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        player.physicsBody?.velocity = CGVector(dx: 0, dy: 0)//makes the game more realistic
+        player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 20))//gives a push upwards
      
     }
     
@@ -43,6 +59,12 @@ class GameScene: SKScene {
         let animation = SKAction.animate(with: [playerTexture, frame2, frame3, frame2], timePerFrame: 0.01)
         let runForever = SKAction.repeatForever(animation) //creates an action that repeats another action forever.
         player.run(runForever)
+        //physics
+        player.physicsBody = SKPhysicsBody(texture: playerTexture, size: playerTexture.size())
+        player.physicsBody!.isDynamic = true //the physics body is moved by the physics simulation.
+        player.physicsBody!.contactTestBitMask = player.physicsBody!.collisionBitMask //whenever the player touches anything the contact is detected
+        player.physicsBody!.collisionBitMask = 0
+    
         
     }
     
@@ -93,6 +115,8 @@ class GameScene: SKScene {
             let ground = SKSpriteNode(texture: groundTexture)
             ground.position = CGPoint(x: (groundTexture.size().width / 2.0 + (groundTexture.size().width * CGFloat(i))), y: groundTexture.size().height / 2)
             ground.zPosition = -10
+            ground.physicsBody = SKPhysicsBody(texture: ground.texture!, size: ground.texture!.size())
+            ground.physicsBody?.isDynamic = false//otherwise the ground would drop off
             addChild(ground)
             //actions
             let moveLeft = SKAction.moveBy(x: -groundTexture.size().width, y: 0, duration: 5)
@@ -114,15 +138,21 @@ class GameScene: SKScene {
         topRock.xScale = -1.0//flip
         topRock.zPosition = -20
         addChild(topRock)
+        topRock.physicsBody = SKPhysicsBody(texture: rockTexture, size: rockTexture.size())
+        topRock.physicsBody!.isDynamic = false
         //bottomRock
         let bottomRock = SKSpriteNode(texture: rockTexture)
         bottomRock.zPosition = -20
         addChild(bottomRock)
-        //used to track the player's score
+        bottomRock.physicsBody = SKPhysicsBody(texture: rockTexture, size: rockTexture.size())
+        bottomRock.physicsBody!.isDynamic = false
+        //red rectangle:used to track the player's score
         let rockCollision = SKSpriteNode(color: UIColor.red, size: CGSize(width: 32, height: frame.height))
-        rockCollision.name = "scoreDetect"
+        rockCollision.physicsBody = SKPhysicsBody(rectangleOf: rockCollision.size)//creates a rectangular physics body centered on the owning node’s origin.
+        rockCollision.physicsBody!.isDynamic = false
+        rockCollision.name = "scoreDetect"//to check  for collision
         addChild(rockCollision)
-        //
+        //positioning
         let xPosition = frame.width + topRock.frame.width
         let max = CGFloat(frame.height / 3)
         let yPosition = CGFloat.random(in: -50...max)
@@ -153,6 +183,7 @@ class GameScene: SKScene {
         
     }
     
+    
     func createScore() {
         
         scoreLabel = SKLabelNode(fontNamed: "Optima-ExtraBlack")
@@ -163,7 +194,48 @@ class GameScene: SKScene {
         addChild(scoreLabel)
         
     }
+   
     
+    
+}
+
+
+
+extension GameScene: SKPhysicsContactDelegate {
+   
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        if contact.bodyA.node?.name == "scoreDetect" || contact.bodyB.node?.name == "scoreDetect" {
+            
+            if contact.bodyA.node == player {
+                contact.bodyB.node?.removeFromParent()
+            } else {
+                contact.bodyA.node?.removeFromParent()
+            }
+            let sound = SKAction.playSoundFileNamed("coin.wav", waitForCompletion: false)
+            run(sound)
+            score += 1
+            return
+            
+        }
+        //avoids 2 collisions being triggered at the same time
+        guard contact.bodyA.node != nil && contact.bodyB.node != nil else { return }
+        //the player hits an obstacle: game over
+        if contact.bodyA.node == player || contact.bodyB.node == player {
+            
+            if let explosion = SKEmitterNode(fileNamed: "PlayerExplosion") {
+                explosion.position = player.position
+                addChild(explosion)
+            }
+            let sound = SKAction.playSoundFileNamed("explosion.wav", waitForCompletion: false)
+            run(sound)
+            player.removeFromParent()
+            speed = 0
+            
+        }
+        
+    }
     
     
     
--- Crashy Plane/GameViewController.swift
+++ Crashy Plane/GameViewController.swift
@@ -17,6 +17,7 @@ class GameViewController: UIViewController {
                 view.presentScene(scene)
             }
             view.ignoresSiblingOrder = true
+            view.showsPhysics = true
         }
         
     }
